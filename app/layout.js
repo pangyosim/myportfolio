190c7218b2a5311ba7230fb8458cfe4c6c85fd1e
PangyoSim <spg9468@naver.com>
2026-03-31 23:36:44 +0900

    [수정] 포트폴리오

--- app/layout.js
+++ app/layout.js
@@ -14,12 +14,35 @@ const pretendard = localFont({
 });
 
 export const metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'https://portfolio.it.kr'),
   title: '심판교 포트폴리오',
-  description: 'AICON과 일독 프로젝트를 중심으로 소개하는 풀스택 개발자 포트폴리오',
+  description: '서비스를 설계하고 개발하는 심판교의 포트폴리오입니다. 여러 프로젝트를 중심으로 다양한 기술 스택과 프로젝트 경험을 소개합니다.',
   icons: {
     icon: '/img/code.png',
     shortcut: '/img/code.png',
     apple: '/img/code.png',
+  },
+  openGraph: {
+    title: '심판교 포트폴리오',
+    description: '서비스를 설계하고 개발하는 심판교의 포트폴리오입니다. 여러 프로젝트를 중심으로 다양한 기술 스택과 프로젝트 경험을 소개합니다.',
+    url: '/',
+    siteName: '심판교 포트폴리오',
+    locale: 'ko_KR',
+    type: 'website',
+    images: [
+      {
+        url: '/img/page_main.png',
+        width: 1200,
+        height: 630,
+        alt: '심판교 포트폴리오 대표 이미지',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '심판교 포트폴리오',
+    description: '서비스를 설계하고 개발하는 심판교의 포트폴리오입니다. 여러 프로젝트를 중심으로 다양한 기술 스택과 프로젝트 경험을 소개합니다.',
+    images: ['/img/page_main.png'],
   },
 };
 
